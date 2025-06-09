87d266e7d622f53722b5e3b73dda12fe9656d36a
Abhishek <abhipandeytech@gmail.com>
2025-06-09 22:08:22 +0530

    Added one handler

--- Project/Project/src/main/java/com/abhishek/Project/HelloController.java
+++ Project/Project/src/main/java/com/abhishek/Project/HelloController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/abc")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/java")
+    public String helloJava() {
+        return "Hello, Java World!";
     }
 }
 
